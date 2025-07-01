c7a2276d38ed057e6d0546286a6587db5008801c
learnwithjacksun <codewithjacksun@gmail.com>
2025-07-01 09:03:17 +0100

    Refactor Notifications component to utilize useNotify hook for dynamic notification rendering

--- client/src/Pages/Dashboard/Notifications.tsx
+++ client/src/Pages/Dashboard/Notifications.tsx
@@ -1,7 +1,9 @@
+import { useNotify } from "@/Hooks";
 import { DashboardLayout } from "@/Layouts";
 import { Tv2 } from "lucide-react";
 
 const Notifications = () => {
+  const { notifications } = useNotify();
   return (
     <>
       <DashboardLayout title="Notifications">
@@ -16,29 +18,33 @@ const Notifications = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="flex items-start gap-4 p-4 border border-line rounded-lg bg-background hover:border-green-800 relative">
-            {/* Read/Unread Indicator */}
-            <span className="absolute top-3 right-3 h-2 w-2 rounded-full bg-green-600"></span>{" "}
-            {/* change to bg-muted for read */}
-            {/* Icon */}
-            <div className="bg-green-800/10 min-h-9 min-w-9 center rounded-md text-green-800">
-              <Tv2 size={20} />
-            </div>
-            {/* Text Content */}
-            <div className="flex-1">
-              <h3 className="font-semibold text-base">System Notification</h3>
-              <p className="text-sm text-muted mt-1">
-                You successfully logged in from a new device. If this wasn't
-                you, please secure your account immediately.
-              </p>
-              <p className="text-xs text-muted mt-2">Just now</p>
+          {notifications?.map((x, y) => (
+            <div
+              key={y}
+              className="flex items-start gap-4 p-4 border border-line rounded-lg bg-background hover:border-green-800 relative"
+            >
+              {/* Read/Unread Indicator */}
+              <span className="absolute top-3 right-3 h-2 w-2 rounded-full bg-green-600"></span>{" "}
+              {/* change to bg-muted for read */}
+              {/* Icon */}
+              <div className="bg-green-800/10 min-h-9 min-w-9 center rounded-md text-green-800">
+                <Tv2 size={20} />
+              </div>
+              {/* Text Content */}
+              <div className="flex-1">
+                <h3 className="font-semibold text-base"> {x.title}</h3>
+                <p className="text-sm text-muted mt-1">{x.description}</p>
+                <p className="text-xs text-muted mt-2">
+                  {new Date(x.$createdAt).toLocaleDateString()}
+                </p>
 
-              {/* Mark as Read Button */}
-              <button className="mt-3 text-xs font-medium text-green-800">
-                Mark as Read
-              </button>
+                {/* Mark as Read Button */}
+                <button className="mt-3 text-xs font-medium text-green-800">
+                  Mark as Read
+                </button>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </DashboardLayout>
     </>
